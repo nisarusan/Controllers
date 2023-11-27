9b1b2906d17e937c698ca092b36fa34e3a958396
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-11-27 04:51:32 +0100

    PersonController updated

--- src/main/java/nl/novi/persons/controller/PersonController.java
+++ src/main/java/nl/novi/persons/controller/PersonController.java
@@ -2,30 +2,38 @@ package nl.novi.persons.controller;
 
 
 import nl.novi.persons.model.Person;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/persons")
 public class PersonController {
 
     //instance fields - retrieve list person objects
     List<Person> personList = new ArrayList<>();
 
-    @GetMapping("/person")
+    @GetMapping
     public ResponseEntity<List<Person>> getMapPerson() {
         return ResponseEntity.ok(personList);
     }
 
-    @PostMapping("/person")
+    @PostMapping
     public ResponseEntity<String> addPerson(@RequestBody Person person) {
         personList.add(person);
-        return ResponseEntity.ok(person.getName() + " is succesfully added");
+        URI location = URI.create("/person/" + person.getId());
+//        Long newPersonId = person.getId();  // Replace with the actual ID logic
+        // Construct the URI for the newly created resource
+//        return ResponseEntity.ok(person.getName() + " is succesfully added");
+        return ResponseEntity.created(location).body(person.getName() + " is succesfully added");
     }
 
-    @PutMapping("/person/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updatePerson(@PathVariable Long id, @RequestBody Person updatedPerson) {
         for (int i = 0; i < personList.size(); i++) {
             Person person = personList.get(i);
@@ -33,19 +41,24 @@ public class PersonController {
                 // Update the person's information, excluding ID from the request body
                 updatedPerson.setId(person.getId());  // Ensure the ID is not changed
                 personList.set(i, updatedPerson);
-                return ResponseEntity.ok("Person updated successfully");
+//                return new ResponseEntity<>(person, HttpStatus.OK);
+                return ResponseEntity.ok(person.getName() + " is updated successfully with: " + updatedPerson.getName());
             }
         }
-        return ResponseEntity.notFound().build();
+        // traditional way of creating a bad request resource by making a new object instance of the response entity
+//        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
+        // this is the the new way with Spring Boot;
+        return ResponseEntity.badRequest().build();
     }
 
     //Delete CRUD
-    @DeleteMapping("/person/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePerson(@PathVariable Long id) {
         for (Person person : personList) {
             if (person.getId().equals(id)) {
                 personList.remove(person);
-                return ResponseEntity.ok("Person with " + id + " succesfully got deleted");
+                return ResponseEntity.ok(person.getName() + " with id " + id + " succesfully got deleted");
             }
         }
         return ResponseEntity.notFound().build();
