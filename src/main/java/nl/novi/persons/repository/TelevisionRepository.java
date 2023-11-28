e782d91bbaba1be9ecee99dc787de4adc52c5d3e
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-11-28 06:37:56 +0100

    Television Controller / Television Repository

---
+++ src/main/java/nl/novi/persons/repository/TelevisionRepository.java
@@ -0,0 +1,9 @@
+package nl.novi.persons.repository;
+
+import nl.novi.persons.model.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+
+
+}
